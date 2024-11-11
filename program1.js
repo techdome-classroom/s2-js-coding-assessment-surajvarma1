ef3565942f23ea11f3da56b26e5155956d311df5
Suraj Kanuganti <115855510+surajvarma1@users.noreply.github.com>
2024-11-11 15:30:36 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const bracketMap = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
     
+    for (let char of s) {
+        if (bracketMap[char]) {
+            stack.push(bracketMap[char]);
+        } else {
+            if (stack.pop() !== char) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
+
 };
 
 module.exports = { isValid };
