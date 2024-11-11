82151cc5a8848b0c2ae13566aa63d0b0f0822ca0
Suraj Kanuganti <115855510+surajvarma1@users.noreply.github.com>
2024-11-11 18:02:17 +0530

    program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,9 @@
-function romanToInt(s) {
-    const romanMap = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
-    };
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanMap = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
     
     let result = 0;
     for (let i = 0; i < s.length; i++) {
@@ -16,4 +18,6 @@ function romanToInt(s) {
     }
     
     return result;
-}
+};
+
+module.exports = { romanToInt };
